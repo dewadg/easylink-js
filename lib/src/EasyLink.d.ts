ad5166f9ef98e79bd742e7deab84746831a8f6b9
Adhyaksa Jaya <adhyaksaj@gmail.com>
2018-09-25 16:17:23 +0700

    Set compiler to target ES5 and remove comments

--- lib/src/EasyLink.d.ts
+++ lib/src/EasyLink.d.ts
@@ -1,77 +1,21 @@
 import { AxiosInstance } from 'axios';
 import { InitialData, UserData } from './interfaces';
 declare class EasyLink {
-    /**
-     * Stores EasyLink SDK host address.
-     */
     host: string;
-    /**
-     * Stores device's serial number.
-     */
     serialNumber: string;
-    /**
-     * Stores the Axios instance.
-     */
     axios: AxiosInstance;
-    /**
-     * Fcking constructor.
-     *
-     * @param param0
-     */
     constructor({host, serialNumber}: InitialData);
-    /**
-     * Generate payload string from object.
-     *
-     * @param payload - Payload object
-     */
     generatePayload(payload: any): string;
-    /**
-     * Get device info.
-     */
     getDeviceInfo(): Promise<any>;
-    /**
-     * Get all users.
-     */
     getAllUsers(): Promise<any>;
-    /**
-     * Upload a user to the device.
-     *
-     * @param data - User data
-     */
     uploadUser(data: UserData): Promise<void>;
-    /**
-     * Delete all users on the device.
-     */
     deleteAllUsers(): Promise<void>;
-    /**
-     * Delete a user.
-     *
-     * @param pin - User PIN
-     */
     deleteUser(pin: string): Promise<void>;
-    /**
-     * Get all scan logs.
-     */
     getAllScanLogs(): Promise<any>;
-    /**
-     * Get new scan logs.
-     */
     getNewScanLogs(): Promise<any>;
-    /**
-     * Delete all scan logs.
-     */
     deleteAllScanLogs(): Promise<void>;
-    /**
-     * Sync date/time.
-     */
     syncDateTime(): Promise<void>;
-    /**
-     * Delete device logs.
-     */
     deleteDeviceLogs(): Promise<void>;
-    /**
-     * Initialize the device.
-     */
     init(): Promise<void>;
 }
 export { EasyLink };
